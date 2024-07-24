0c3b4c9869e0e21568aa777ff7221e3056441a47
bhaskardixit <bhaskar.dixit@xebia.com>
2024-07-24 16:53:23 +0530

    feat: angular keycloak basic setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,33 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { OAuthService } from 'angular-oauth2-oidc';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'rental-products-ui';
+  helloText = '';
+
+  constructor(private oauthService: OAuthService, private httpClient: HttpClient) { }
+
+  logout() {
+    console.log("logging out", this.oauthService);
+    this.oauthService.logOut();
+  }
+
+  getHelloText() {
+    this.httpClient.get<{ message: string }>('http://localhost:8080/hello', {
+      headers: {
+        'Authorization': `Bearer ${this.oauthService.getAccessToken()}`
+      }
+    }).subscribe(result => {
+      this.helloText = result.message;
+    });
+  }
 }
