cdde4c5a9321975dbb72e22b83928632d04a5739
devBhaskar <officialbhaskar@outlook.com>
2024-09-16 22:48:45 +0530

    feat: footer UI

--- src/app/pages/footer/footer.component.ts
+++ src/app/pages/footer/footer.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, ElementRef, inject, Renderer2, ViewChild, AfterViewInit} from '@angular/core';
 
 @Component({
   selector: 'app-footer',
@@ -7,6 +7,28 @@ import {Component} from '@angular/core';
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss',
 })
-export class FooterComponent {
+export class FooterComponent implements AfterViewInit {
+  private readonly renderer = inject(Renderer2);
   currentYear: string = '2024';
+
+  @ViewChild('footer', { static: false }) footerElement!: ElementRef;
+
+  footerHeight = 100;
+
+  ngAfterViewInit(): void {
+    // Get the footer height dynamically after the view is initialized
+    if (this.footerElement) {
+      this.footerHeight = this.footerElement.nativeElement.offsetHeight;
+      this.adjustSidenavHeight();
+    }
+  }
+
+  adjustSidenavHeight() {
+    const container = document.querySelector('mat-sidenav-container') as HTMLElement;
+    if (container) {
+      const availableHeight = `calc(100vh - ${this.footerHeight * 2.5}px)`;
+      this.renderer.setStyle(container, 'height', availableHeight);
+    }
+  }
+
 }
